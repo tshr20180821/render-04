7f18d8e3525eb5c7d595640f456a03314625dd05
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-13 04:23:47 +0900

    Update MyUtils.js

--- app/MyUtils.js
+++ app/MyUtils.js
@@ -32,32 +32,36 @@ class MyLog {
     #output(level_, message_) {
         new Promise((resolve) => {
             // console.log((new Error).stack);
-            const match = (new Error()).stack.split("\n")[5].substring(7).match(this._regex);
+            try {
+                const match = (new Error()).stack.split("\n")[5].substring(7).match(this._regex);
 
-            const dt = new Date();
-            const datetime = dt.getFullYear() + '-' + ('0' + (dt.getMonth() + 1)).slice(-2) + '-' + ('0' + dt.getDate()).slice(-2) + ' ' +
-                ('0' + dt.getHours()).slice(-2) + ':' + ('0' + dt.getMinutes()).slice(-2) + ':' + ('0' + dt.getSeconds()).slice(-2) + '.' +
-                ('00' + dt.getMilliseconds()).slice(-3);
-            const log_header = process.env.RENDER_EXTERNAL_HOSTNAME + ' ' + process.env.DEPLOY_DATETIME + ' ' +
-                process.pid + ' ' + level_ + ' ' + match[2] + ' ' + match[3] + ' [' + match[1] + ']';
-            console.log(datetime + ' \x1b[35m' + log_header + '\x1b[0m ' + message_);
-            const loggly_options = {
-                protocol: 'https:',
-                port: 443,
-                hostname: 'logs-01.loggly.com',
-                path: '/inputs/' + process.env.LOGGLY_TOKEN +
-                    '/tag/' + process.env.RENDER_EXTERNAL_HOSTNAME + ',' + process.env.RENDER_EXTERNAL_HOSTNAME + '_' + process.env.DEPLOY_DATETIME + ',' + level_ + ',nodejs/',
-                method: 'POST',
-                headers: {
-                    'content-type': 'text/plain; charset=utf-8',
-                }
-            };
-            loggly_options.agent = new https.Agent({
-                keepAlive: true
-            });
-            const request = https.request(loggly_options);
-            request.write(datetime + ' ' + log_header + ' ' + message_);
-            request.end();
+                const dt = new Date();
+                const datetime = dt.getFullYear() + '-' + ('0' + (dt.getMonth() + 1)).slice(-2) + '-' + ('0' + dt.getDate()).slice(-2) + ' ' +
+                    ('0' + dt.getHours()).slice(-2) + ':' + ('0' + dt.getMinutes()).slice(-2) + ':' + ('0' + dt.getSeconds()).slice(-2) + '.' +
+                    ('00' + dt.getMilliseconds()).slice(-3);
+                const log_header = process.env.RENDER_EXTERNAL_HOSTNAME + ' ' + process.env.DEPLOY_DATETIME + ' ' +
+                    process.pid + ' ' + level_ + ' ' + match[2] + ' ' + match[3] + ' [' + match[1] + ']';
+                console.log(datetime + ' \x1b[35m' + log_header + '\x1b[0m ' + message_);
+                const loggly_options = {
+                    protocol: 'https:',
+                    port: 443,
+                    hostname: 'logs-01.loggly.com',
+                    path: '/inputs/' + process.env.LOGGLY_TOKEN +
+                        '/tag/' + process.env.RENDER_EXTERNAL_HOSTNAME + ',' + process.env.RENDER_EXTERNAL_HOSTNAME + '_' + process.env.DEPLOY_DATETIME + ',' + level_ + ',nodejs/',
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'text/plain; charset=utf-8',
+                    }
+                };
+                loggly_options.agent = new https.Agent({
+                    keepAlive: true
+                });
+                const request = https.request(loggly_options);
+                request.write(datetime + ' ' + log_header + ' ' + message_);
+                request.end();
+            } catch (err) {
+                console.log(err.toString());
+            }
             resolve();
         });
     }
@@ -78,18 +82,20 @@ module.exports.send_slack_message = function (message_) {
             'Content-type': 'application/json'
         }
     };
-
-    [process.env.SLACK_CHANNEL_01, process.env.SLACK_CHANNEL_02].forEach(channel => {
-        var post_data = JSON.stringify({
-            text: message_,
-            channel: channel
+    new Promise((resolve) => {
+        [process.env.SLACK_CHANNEL_01, process.env.SLACK_CHANNEL_02].forEach(channel => {
+            const post_data = JSON.stringify({
+                text: message_,
+                channel: channel
+            });
+            const request = require('https').request('https://slack.com/api/chat.postMessage', http_options, response => {
+                logger.info('Slack Post Message Result : ' + response.statusCode);
+            });
+            request.write(post_data);
+            request.end();
+            sleep_ms(1000);
         });
-        var request = require('https').request('https://slack.com/api/chat.postMessage', http_options, response => {
-            logger.info('Slack Post Message Result : ' + response.statusCode);
-        });
-        request.write(post_data);
-        request.end();
-        sleep_ms(1000);
+        resolve();
     });
 }
 
