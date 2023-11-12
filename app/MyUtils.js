449875b97fa095471b459ae746857956e1ef53a4
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-13 08:07:11 +0900

    Update MyUtils.js

--- app/MyUtils.js
+++ app/MyUtils.js
@@ -35,6 +35,10 @@ class MyLog {
             try {
                 const match = (new Error()).stack.split("\n")[5].substring(7).match(this._regex);
 
+                if (message_ == null) {
+                    message_ = "";
+                }
+                
                 const dt = new Date();
                 const datetime = dt.getFullYear() + '-' + ('0' + (dt.getMonth() + 1)).slice(-2) + '-' + ('0' + dt.getDate()).slice(-2) + ' ' +
                     ('0' + dt.getHours()).slice(-2) + ':' + ('0' + dt.getMinutes()).slice(-2) + ':' + ('0' + dt.getSeconds()).slice(-2) + '.' +
