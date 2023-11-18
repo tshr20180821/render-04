4c6c66083f23221840efbcefca5146e5851bdeb1
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-19 06:44:33 +0900

    Update log_memcached.sh

--- app/log_memcached.sh
+++ app/log_memcached.sh
@@ -3,5 +3,9 @@
 PID=$$
 
 while read line; do
-echo "${PID} Memcached Log : ${line}"
+  dt=$(date '+%Y-%m-%d %H:%M:%S.%3N')
+  curl -X POST -s -o /dev/null -H "Content-Type: text/plain" -d "${dt} ${RENDER_EXTERNAL_HOSTNAME} ${DEPLOY_DATETIME} ${pid} Memcached ${line}" \
+    "https://logs-01.loggly.com/inputs/${LOGGLY_TOKEN}/tag/${RENDER_EXTERNAL_HOSTNAME},${RENDER_EXTERNAL_HOSTNAME}_${DEPLOY_DATETIME},Memcached,${1}/" &
+
+  echo "${dt} ${RENDER_EXTERNAL_HOSTNAME} ${DEPLOY_DATETIME} ${pid} Memcached ${line}"
 done
