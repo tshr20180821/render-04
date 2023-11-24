efa27898e51e73112968837a4b411161c6ff3bc2
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-24 16:08:25 +0900

    Update preload.php

--- auth/preload.php
+++ auth/preload.php
@@ -3,17 +3,17 @@
 $rc = opcache_compile_file('/usr/src/app/log.php');
 error_log("log.php : " . $rc);
 
-$rc = opcache_compile_file('/var/wwww/html/auth/crond.php');
+$rc = opcache_compile_file('/var/www/html/auth/crond.php');
 error_log("crond.php : " . $rc);
 
-$rc = opcache_compile_file('/var/wwww/html/auth/health_check.php');
+$rc = opcache_compile_file('/var/www/html/auth/health_check.php');
 error_log("health_check.php : " . $rc);
 
-$rc = opcache_compile_file('/var/wwww/html/auth/get_dockerhub_repository_information.php');
+$rc = opcache_compile_file('/var/www/html/auth/get_dockerhub_repository_information.php');
 error_log("get_dockerhub_repository_information.php : " . $rc);
 
-$rc = opcache_compile_file('/var/wwww/html/auth/exec_log_operation.php');
+$rc = opcache_compile_file('/var/www/html/auth/exec_log_operation.php');
 error_log("exec_log_operation.php : " . $rc);
 
-$rc = opcache_compile_file('/var/wwww/html/auth/update_sqlite.php');
+$rc = opcache_compile_file('/var/www/html/auth/update_sqlite.php');
 error_log("update_sqlite.php : " . $rc);
