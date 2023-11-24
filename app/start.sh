470ba1f95a74b8fcc64e93f168aa08e54a26c249
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-24 16:16:47 +0900

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -94,6 +94,8 @@ curl -sS -X POST -H "Authorization: Bearer ${SLACK_TOKEN}" \
 . /etc/apache2/envvars >/dev/null 2>&1
 exec /usr/sbin/apache2 -DFOREGROUND &
 
+sleep 5s && curl -u ${BASIC_USER}:${BASIC_PASSWORD} http://127.0.0.1/auth/preload.php &
+
 # for i in {1..60} ; do sleep 30s && ps aux ; done &
 while true; do sleep 900s && ps aux; done &
 
