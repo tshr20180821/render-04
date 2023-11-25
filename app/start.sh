bf0e8c4077c2a2322c56f1c00109609dd954eaf2
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-26 07:35:26 +0900

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -45,8 +45,9 @@ npm list --depth=0
 
 # memcached sasl
 useradd memcached -G sasl
+export MEMCACHED_SASL_PWDB=/tmp/sasl.db
 export SASL_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
-echo ${SASL_PASSWORD} | saslpasswd2 -p -a memcached -c memcached
+echo ${SASL_PASSWORD} | saslpasswd2 -p -a memcached -c memcached -f ${MEMCACHED_SASL_PWDB}
 chown memcached:memcached /etc/sasldb2
 # sasldblistusers2
 export SASL_CONF_PATH=/tmp/memcached.conf
