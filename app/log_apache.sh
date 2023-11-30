d96b2046ffba320fbbea8de726d9abe6fde62502
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-30 11:20:56 +0900

    Update log_apache.sh

--- app/log_apache.sh
+++ app/log_apache.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+url="https://logs-01.loggly.com/inputs/${LOGGLY_TOKEN}/tag/${RENDER_EXTERNAL_HOSTNAME},${RENDER_EXTERNAL_HOSTNAME}_${DEPLOY_DATETIME},apache,${1}/"
+
 while read line; do
-  curl -X POST -s -o /dev/null -H "Content-Type: text/plain" -d "${line}" \
-    "https://logs-01.loggly.com/inputs/${LOGGLY_TOKEN}/tag/${RENDER_EXTERNAL_HOSTNAME},${RENDER_EXTERNAL_HOSTNAME}_${DEPLOY_DATETIME},apache,${1}/" &
+  curl -X POST -s -o /dev/null -H "Content-Type: text/plain" -d "${line}" "${url}"
   if [ "${1}" = "access" ]; then
     echo "${line}" | sed 's/\(HTTP\/1..\) \([0-9][0-9][0-9]\)/\1 \x1b[36m\2\x1b[0m/'
   else
