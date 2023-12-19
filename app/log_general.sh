f417064a5b170864d4fcdb1f9afdb1c8026e6d0f
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-12-19 12:42:39 +0900

    Update log_general.sh

--- app/log_general.sh
+++ app/log_general.sh
@@ -4,7 +4,7 @@ pid=$$
 string_part_1="${RENDER_EXTERNAL_HOSTNAME} ${DEPLOY_DATETIME} ${pid} ${1}"
 url="https://logs-01.loggly.com/inputs/${LOGGLY_TOKEN}/tag/${RENDER_EXTERNAL_HOSTNAME},${RENDER_EXTERNAL_HOSTNAME}_${DEPLOY_DATETIME},${1}/"
 
-while read line; do
+while read -r line; do
   dt=$(date '+%Y-%m-%d %H:%M:%S.%3N')
   string_part_2="${dt} ${string_part_1} ${line}"
   # curl -X POST -s -o /dev/null -H "Content-Type: text/plain" -d "${dt} ${RENDER_EXTERNAL_HOSTNAME} ${DEPLOY_DATETIME} ${pid} ${1} ${line}" \
