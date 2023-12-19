1df8d3360d5354f021940f3c22a0dc8beff7ab94
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-12-19 09:10:55 +0900

    Update check_backports.sh

--- app/check_backports.sh
+++ app/check_backports.sh
@@ -4,4 +4,6 @@ set +x
 
 echo "check ${1}" | tee -a "${2}"
 curl -sS -m 10 "https://packages.debian.org/bookworm-backports/${1}" 2>>"${2}" | grep '<h1>' | grep "${1}" | cut -c 14- | tee -a "${2}"
+# grep '<h1>'
+# <h1>Package: curl (8.4.0-2~bpo12+1 and others)
 sleep 2s
